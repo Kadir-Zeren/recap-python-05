75e6bba866a45acee9921e58d3a7ec6cfc5473e6
kadir-zeren <k4dzeren@gmail.com>
2024-12-24 12:11:30 +0300

    practice

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
 text = 'AaBbCc'
 print(text.lower())
+
+cumle = 'In God We Trust'
+cumle.lower()
+
+
+text = 'tyou can learn almost everything in pre-classz'
+print(text.rstrip('z').lstrip('t').upper())
